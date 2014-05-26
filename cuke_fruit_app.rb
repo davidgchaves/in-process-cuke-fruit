f54d28b1801d2a9a3039ae4e885fc4491daeb39f
David G Chaves <davidgchaves@gmail.com>
2014-05-26 11:33:50 +0200

    The response is json

--- cuke_fruit_app.rb
+++ cuke_fruit_app.rb
@@ -2,4 +2,9 @@ require 'sinatra'
 
 class CukeFruitApp < Sinatra::Base
   set :data, ''
+
+  get "/fruits" do
+    content_type :json
+    CukeFruitApp.data.to_json
+  end
 end
--- features/step_defintions/rest_steps.rb
+++ features/step_defintions/rest_steps.rb
@@ -2,6 +2,6 @@ When /^the client requests GET (.*)$/ do |path|
   get path
 end
 
-Then(/^the response should be JSON:$/) do |json|
-  pending # express the regexp above with the code you wish you had
+Then /^the response should be JSON:$/ do |json|
+  expect(JSON.parse last_response.body).to eq JSON.parse(json)
 end
